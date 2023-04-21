438c15c5e90f890f02a133b5da8c13accdd69d95
Thanuseng Kevin Prachith <tkprachith@gmail.com>
2023-04-20 23:32:14 -0400

    Adds comments to ListGroup code

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import ListGroup from "./components/ListGroup";
 
 function App() {
-  return <div>
-      <ListGroup />
+  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // test data for the population of the unordered list
+
+  const handleSelectItem = (item: string) => {
+    console.log(item);
+  };
+
+  return (
+    <div>
+      <ListGroup
+        items={items} // passing in the items array above
+        heading="Cities" // passing in the string "Cities"
+        onSelectItem={handleSelectItem} // in the component, there is a function called onSelectItem that takes in an item and returns void. Here, when the function onSelectItem is run, it also runs the handler function defined above, which prints the item to the console
+      />
     </div>
+  );
 }
 
 export default App;
--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -1,21 +1,30 @@
-import { MouseEvent } from "react"; // allows for type definition on line 7, feature of typescript
+import { useState } from "react"; // This allows us to use the useState hook from the react module
 
-function ListGroup() {
-  let items = ["New York", "San Francisco", "Tokyo", "London", "Paris"]; // test data for the population of the unordered list
+interface Props {
+    items: string[]; // items is declared as an array of strings
+    heading: string; // heading is declared as a string
+    onSelectItem: (item: string) => void; // onSelectItem is a function that takes in an item of type string and returns void - allows for us to accept information from the parent component!
+}
 
-  // Event handler: whenever we want to handle an event (in this case, onClick of the list items), we want to move the logic into its own function to improve readability
-  const handleClick = (event: MouseEvent) => console.log(event);
+// Here, we are defining the ListGroup component and listing the props that it takes in - all components have a props property, and this syntax is destructuring/pulling just the listed properties from there. After the colon is syntax for TypeScript where we explicitly declare the type as Props - this is an optional add on step but helps with catching a lot of potential errors before runtime. This Props type is declared above in an interface, where we list the props and declare what type of value that prop receives
+function ListGroup({ items, heading, onSelectItem }: Props) {
+
+  // useState Hook - the first position of the index is a stored value, and the second position is a function used to update that value. The stored value should never be mutated directly, only through use of the updater function! On the right hand side, we can set the default/initial value.
+  const [selectedIndex, setSelectedIndex] = useState(-1);
 
   return (
     <>
-      <h1>List</h1>
+    {/* heading is dynamic, changed based on the value passed into the component */}
+      <h1>{heading}</h1>
+      {/* This is a common structure used to catch a default value - in this care, if the length of our items array is 0, then that side evaluates to TRUE and returns the second part of the statement after the && */}
       {items.length === 0 && <p>No item found</p>}
       <ul className="list-group">
         {items.map((item, index) => (
           <li
-            className="list-group-item"
+            className={ selectedIndex === index ? 'list-group-item active' : 'list-group-item' }
             key={item}
-            onClick={handleClick} // important to note, this function is not called here! It will only be called/invoked when the click event happens
+            onClick={() => { setSelectedIndex(index) // when clicked, this function runs and the index of the clicked list item is passed to the setSelectedIndex function, which serves to update the selectedIndex value
+            onSelectItem(item)}} // this function is used in the parent component to pass in a handler function - end goal is taking the item that is clicked and printing it to console
           >
             {item}
           </li>
